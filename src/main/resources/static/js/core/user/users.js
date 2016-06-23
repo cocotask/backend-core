d2743293dea8d6c3934970551c1d6d14ad05bc16
jinhyun <free1330@gmail.com>
2016-06-24 00:36:31 +0900

    feature: 사용자 삭제

--- src/main/resources/static/js/core/user/users.js
+++ src/main/resources/static/js/core/user/users.js
@@ -2,6 +2,34 @@ var usersInit = function() {
   readUsers();
 };
 
+var bind = function () {
+  var rows = document.getElementsByClassName("_deleteUserBtn");
+
+  for (var i = 0; i < rows.length; i++) {
+    rows[i].addEventListener("click", deleteUser);
+  }
+};
+
+var deleteUser = function () {
+  var xhr, response, userUid;
+
+  userUid = this.dataset.userUid;
+
+  xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "http://localhost:8081/rest/users/" + userUid, true);
+  xhr.send();
+
+  xhr.onload = function() {
+    if (xhr.status == '200') {
+      readUsers();
+
+    } else {
+      response = JSON.parse(xhr.response);
+      console.log(response.status + "\n" + response.message);
+    }
+  }
+};
+
 var readUsers = function() {
   var xhr, response;
   xhr = new XMLHttpRequest();
@@ -32,6 +60,8 @@ var showUsers = function(data) {
       users = { "users" : data };
       rendered = Mustache.render(xhr.response, users);
       document.getElementById("usersArticle").innerHTML = rendered;
+
+      bind();
     }
   }
 };
