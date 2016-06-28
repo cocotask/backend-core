0fb0ffc87ee144f635ee3e7c150101e6baef9909
jinhyun <free1330@gmail.com>
2016-06-28 21:51:07 +0900

    refactor: layout.js (module pattern)

--- src/main/resources/static/js/core/layout/layout.js
+++ src/main/resources/static/js/core/layout/layout.js
@@ -1,22 +1,22 @@
-(function(){
-  showLayout();
-})();
-
-function showLayout() {
-  showHeader();
-  showNav();
-
-  function showHeader () {
-    var headerContents;
-    headerContents = document.getElementById("layout_contents").import.getElementById("header_section").innerHTML;
+var layout = function() {
+  var showHeader = function () {
+    var headerContents = document.getElementById("layout_contents").import.getElementById("header_section").innerHTML;
 
     document.getElementsByTagName("header")[0].innerHTML = headerContents;
-  }
+  };
 
-  function showNav () {
-    var navContents;
-    navContents = document.getElementById("layout_contents").import.getElementById("nav_section").innerHTML;
+  var showNav = function () {
+    var navContents = document.getElementById("layout_contents").import.getElementById("nav_section").innerHTML;
 
     document.getElementsByTagName("nav")[0].innerHTML = navContents;
+  };
+
+  return {
+    showHeaderNav: function() {
+      showHeader();
+      showNav();
+    }
   }
-};
+}();
+
+layout.showHeaderNav();
