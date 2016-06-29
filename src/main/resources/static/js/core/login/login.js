9af5fb4110555f2f7b1b6e91e2a6141c60c310c4
jinhyun <free1330@gmail.com>
2016-06-29 13:44:18 +0900

    refactor: login

--- src/main/resources/static/js/core/login/login.js
+++ src/main/resources/static/js/core/login/login.js
@@ -1,66 +1,71 @@
-var loginInit = function () {
-  bind();
-};
-
-var bind = function () {
-  document.getElementById("submitLoginBtn").addEventListener("click", submitLogin);
-  document.getElementById("submitLogoutBtn").addEventListener("click", submitLogout);
-};
-
-var submitLogin = function () {
-  var xhr, login, response;
-
-  login = {
-    userEmail: document.getElementById("userEmail").value,
-    password: document.getElementById("password").value
+var login = function () {
+  var bind = function () {
+    document.getElementById("submitLoginBtn").addEventListener("click", submitLogin);
+    document.getElementById("submitLogoutBtn").addEventListener("click", submitLogout);
   };
 
-  xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:8081/rest/login", true);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.withCredentials = true;
+  var submitLogin = function () {
+    var xhr, login, response;
 
-  xhr.send(JSON.stringify(login));
+    login = {
+      userEmail: document.getElementById("userEmail").value,
+      password: document.getElementById("password").value
+    };
 
-  xhr.onload = function () {
-    if(xhr.status == "200") {
-      response = JSON.parse(xhr.response);
-      document.getElementById("msg").innerText = response.userName + "님 이 로그인 하였습니다.";
-      console.log(response);
+    xhr = new XMLHttpRequest();
+    xhr.open("POST", serverInfo.getRestUrl() + "/login", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.withCredentials = true;
 
-    } else {
-      response = JSON.parse(xhr.response);
-      document.getElementById("msg").innerText = response.status + " : " + response.message;
-      console.log(response.status + " : " + response.message);
+    xhr.send(JSON.stringify(login));
+
+    xhr.onload = function () {
+      if(xhr.status == "200") {
+        response = JSON.parse(xhr.response);
+        document.getElementById("msg").innerText = response.userName + "님 이 로그인 하였습니다.";
+        console.log(response);
+
+      } else {
+        response = JSON.parse(xhr.response);
+        document.getElementById("msg").innerText = response.status + " : " + response.message;
+        console.log(response.status + " : " + response.message);
+      }
+    };
+
+    xhr.onerror = function () {
+      serverInfo.getRestUrl() + "/login"
+      document.getElementById("msg").innerText = serverInfo.getRestUrl() + "/login 서버가 연결되지 않습니다.";
+    };
+  };
+
+  var submitLogout = function () {
+    var xhr, response;
+
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", serverInfo.getRestUrl() + "/logout", true);
+    xhr.withCredentials = true;
+
+    xhr.send();
+
+    xhr.onload = function () {
+      if(xhr.status == "200") {
+        document.getElementById("msg").innerText = "로그아웃 하였습니다.";
+
+      } else {
+        response = JSON.parse(xhr.response);
+        document.getElementById("msg").innerText = response.status + " : " + response.message;
+        console.log(response.status + " : " + response.message);
+      }
+    };
+
+    xhr.onerror = function () {
+      document.getElementById("msg").innerText = serverInfo.getRestUrl() + "/logout 서버가 연결되지 않습니다.";
+    };
+  };
+
+  return {
+    bind: function () {
+      bind();
     }
-  };
-
-  xhr.onerror = function () {
-    document.getElementById("msg").innerText = "http://localhost:8081/rest/login 서버가 연결되지 않습니다.";
-  };
-};
-
-var submitLogout = function () {
-  var xhr, response;
-
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://localhost:8081/rest/logout", true);
-  xhr.withCredentials = true;
-
-  xhr.send();
-
-  xhr.onload = function () {
-    if(xhr.status == "200") {
-      document.getElementById("msg").innerText = "로그아웃 하였습니다.";
-
-    } else {
-      response = JSON.parse(xhr.response);
-      document.getElementById("msg").innerText = response.status + " : " + response.message;
-      console.log(response.status + " : " + response.message);
-    }
-  };
-
-  xhr.onerror = function () {
-    document.getElementById("msg").innerText = "http://localhost:8081/rest/login 서버가 연결되지 않습니다.";
-  };
-};
+  }
+}();
