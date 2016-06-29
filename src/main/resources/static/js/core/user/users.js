203b5f74d8303f41630734ae2a87e2b6aa2c8a08
jinhyun <free1330@gmail.com>
2016-06-29 11:32:10 +0900

    refactor: module

--- src/main/resources/static/js/core/user/users.js
+++ src/main/resources/static/js/core/user/users.js
@@ -1,87 +1,99 @@
-var usersInit = function() {
-  readUsers();
-};
+var users = function () {
+  var bind = function () {
+    var readRows = document.getElementsByClassName("_readUserBtn");
+    var updateRows = document.getElementsByClassName("_updateUserBtn");
+    var deleteRows = document.getElementsByClassName("_deleteUserBtn");
 
-var gotoReadUser = function () {
-  var userUid = this.dataset.userUid;
-  location.href = "http://localhost:8080/web/users/" + userUid;
-};
+    for (var i = 0; i < readRows.length; i++) {
+      readRows[i].addEventListener("click", gotoReadUser);
+    }
 
-var gotoUpdateUser = function () {
-  var userUid = this.dataset.userUid;
-  location.href = "http://localhost:8080/web/users/" + userUid + "/edit";
-};
+    for (var i = 0; i < updateRows.length; i++) {
+      updateRows[i].addEventListener("click", gotoUpdateUser);
+    }
 
-var deleteUser = function () {
-  var xhr, response, userUid;
+    for (var i = 0; i < deleteRows.length; i++) {
+      deleteRows[i].addEventListener("click", deleteUser);
+    }
+  };
 
-  userUid = this.dataset.userUid;
+  var deleteUser = function () {
+    var xhr, response, userUid;
 
-  xhr = new XMLHttpRequest();
-  xhr.open("DELETE", "http://localhost:8081/rest/users/" + userUid, true);
-  xhr.send();
+    userUid = this.dataset.userUid;
 
-  xhr.onload = function() {
-    if (xhr.status == '200') {
+    xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "http://localhost:8081/rest/users/" + userUid, true);
+    xhr.send();
+
+    xhr.onload = function() {
+      if (xhr.status == '200') {
+        readUsers();
+
+      } else {
+        response = JSON.parse(xhr.response);
+        console.log(response.status + "\n" + response.message);
+      }
+    }
+  };
+
+  var readUsers = function() {
+    var xhr, response;
+    xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://localhost:8081/rest/users", true);
+    xhr.send();
+
+    xhr.onload = function() {
+      if (xhr.status == '200') {
+        response = JSON.parse(xhr.response);
+
+        showUsers(response);
+
+      } else {
+        response = JSON.parse(xhr.response);
+        console.log(response.status + "\n" + response.message);
+      }
+    }
+  };
+
+  var showUsers = function(data) {
+    var xhr, users, rendered;
+
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", "/fragments/core/user/users.mustache", true);
+    xhr.send();
+
+    xhr.onload = function() {
+      if (xhr.status == '200') {
+        users = { "users" : data };
+        rendered = Mustache.render(xhr.response, users);
+        document.getElementById("usersArticle").innerHTML = rendered;
+
+        bind();
+      }
+    }
+  };
+
+  var gotoReadUser = function () {
+    var userUid = this.dataset.userUid;
+    location.href = "http://localhost:8080/web/users/" + userUid;
+  };
+
+  var gotoUpdateUser = function () {
+    var userUid = this.dataset.userUid;
+    location.href = "http://localhost:8080/web/users/" + userUid + "/edit";
+  };
+
+  return {
+    usersInit: function() {
       readUsers();
-
-    } else {
-      response = JSON.parse(xhr.response);
-      console.log(response.status + "\n" + response.message);
+    },
+    gotoReadUser: function() {
+      gotoReadUser();
+    },
+    gotoUpdateUser: function() {
+      gotoUpdateUser();
     }
   }
-};
-
-var readUsers = function() {
-  var xhr, response;
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://localhost:8081/rest/users", true);
-  xhr.send();
-
-  xhr.onload = function() {
-    if (xhr.status == '200') {
-      response = JSON.parse(xhr.response);
-      showUsers(response);
-
-    } else {
-      response = JSON.parse(xhr.response);
-      console.log(response.status + "\n" + response.message);
-    }
-  }
-};
-
-var showUsers = function(data) {
-  var xhr, users, rendered;
-
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", "/fragments/core/user/users.mustache", true);
-  xhr.send();
-
-  xhr.onload = function() {
-    if (xhr.status == '200') {
-      users = { "users" : data };
-      rendered = Mustache.render(xhr.response, users);
-      document.getElementById("usersArticle").innerHTML = rendered;
-
-      bind();
-    }
-  }
-};
-
-var bind = function () {
-  var readRows = document.getElementsByClassName("_readUserBtn");
-  var updateRows = document.getElementsByClassName("_updateUserBtn");
-  var deleteRows = document.getElementsByClassName("_deleteUserBtn");
-
-  for (var i = 0; i < readRows.length; i++) {
-    readRows[i].addEventListener("click", gotoReadUser);
-  }
-
-  for (var i = 0; i < updateRows.length; i++) {
-    updateRows[i].addEventListener("click", gotoUpdateUser);
-  }
-
-  for (var i = 0; i < deleteRows.length; i++) {
-    deleteRows[i].addEventListener("click", deleteUser);
-  }
-};
+}();
