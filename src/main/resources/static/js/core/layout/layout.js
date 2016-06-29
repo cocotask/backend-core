5ba625a3377bc681c0f2e8eefc1a06cf33529ebe
jinhyun <free1330@gmail.com>
2016-06-29 15:19:23 +0900

    feature: login in header

--- src/main/resources/static/js/core/layout/layout.js
+++ src/main/resources/static/js/core/layout/layout.js
@@ -11,12 +11,63 @@ var layout = function() {
     document.getElementsByTagName("nav")[0].innerHTML = navContents;
   };
 
+  var loginoutBind = function () {
+    document.getElementById("login").addEventListener("click", showModalLogin);
+    document.getElementById("logout").addEventListener("click", logout);
+  };
+
+  var showLoginContents = function () {
+    var xhr, loginContents;
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", webServer.getUrl() + "/login");
+    xhr.send();
+    xhr.onload = function () {
+      var parser, doc;
+      parser = new DOMParser();
+      doc = parser.parseFromString(xhr.responseText, "text/html");
+
+      loginContents = document.getElementById("login_contents");
+      loginContents.innerHTML = doc.getElementById("login_section").innerHTML;
+
+      login.bind();
+    }
+  };
+
+  var showModalLogin = function () {
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  };
+
+  var logout = function () {
+
+  };
+
   return {
     showHeaderNav: function() {
       showHeader();
       showNav();
+    },
+    showLoginContents : function() {
+      showLoginContents();
+    },
+
+    loginoutBind: function () {
+      loginoutBind();
     }
   }
 }();
 
 layout.showHeaderNav();
+layout.showLoginContents();
+layout.loginoutBind();
